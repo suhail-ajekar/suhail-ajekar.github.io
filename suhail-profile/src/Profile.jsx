4b0469fb3a3aa938a82bce5b8dda960f00a1afe6
SA049914 <suhail.a@cerner.com>
2019-10-13 22:07:21 +0530

    created react app

---
+++ suhail-profile/src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Profile extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return(
+      <h1> About Suhail </h1>
+    );
+  }
+
+}
